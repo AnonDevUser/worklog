e72ccf245a370c6138d95dfb2929b808b55241ca
AarenNepal <nepalaaren63@gmail.com>
2025-12-20 18:32:34 +0545

    Render postgres integration

--- logbook/settings.py
+++ logbook/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
-    'api',
     'rest_framework',
+    'sslserver'
    # 'sslserver', will need later
 ]
 
@@ -79,8 +83,16 @@ WSGI_APPLICATION = 'logbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'logbook_egv0',
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"), 
+        'PORT': os.getenv("DB_PORT"),
+        'CONN_MAX_AGE': 600,   
+        'OPTIONS': {
+            'sslmode': 'require'  
+        },
     }
 }
 
